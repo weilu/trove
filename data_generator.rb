03dbd5bdc34c9770359484fd67219d98ad5e96ad
Wei <luwei.here@gmail.com>
2013-09-25 17:08:23 +0800

    populate release column

--- data_generator.rb
+++ data_generator.rb
@@ -1,14 +1,19 @@
 require 'csv'
+require 'pivotal-tracker'
 
 class DataGenerator
 
   def initialize stories
     @stories = stories
+    @releases = stories.select{|s| s.story_type == 'release'}
   end
 
   def generate
     CSV.generate do |csv|
       csv << %w(release tag status stories)
+      @releases.each do |r|
+        csv << [ r.name ]
+      end
     end
   end
 
--- spec/data_generator_spec.rb
+++ spec/data_generator_spec.rb
@@ -2,7 +2,7 @@ require_relative '../data_generator'
 require 'pry'
 
 describe DataGenerator do
-  let(:stories) { YAML.load('fixtures/stories.yaml') }
+  let(:stories) { YAML.load_file('fixtures/stories.yaml') }
   let(:generator) { described_class.new stories }
 
   describe '#generate' do
@@ -10,6 +10,16 @@ describe DataGenerator do
 
     it 'generates headers' do
       expect(data[0]).to eq %w(release tag status stories)
+    end
+
+    it 'populates release column' do
+      expect(data[1..-1].map(&:first).uniq).to eq(
+        [
+          "Initial demo to investors",
+          "Beta launch",
+          "Full production launch"
+        ]
+      )
     end
   end
 end
